7f84547325faa2cfabe42119e79546594e2ab53c
Marin Sokol <marinsokol5@gmail.com>
2026-07-19 19:00:47 +0200

    Prepare Trading212 Andon Cord 0.3.0 cask

--- Casks/trading212-andon-cord.rb
+++ Casks/trading212-andon-cord.rb
@@ -1,8 +1,8 @@
 cask "trading212-andon-cord" do
-  version "0.2.0"
+  version "0.3.0"
   # `Scripts/publish.sh` replaces this with the release archive's SHA-256
   # before creating the tag. The source cask is not published as-is.
-  sha256 "bfdbfbcda0855afbb4d835a5c0ea9241db650ba43d1c193f5941e7fedf519cdf"
+  sha256 "f19c091861491a9528856d3d640aa781d75506140bd676ce8041065806392c1e"
 
   url "https://github.com/marinsokol5/trading212-andon-cord/releases/download/v#{version}/Trading212-Andon-Cord-#{version}.zip"
   name "Trading212 Andon Cord"
